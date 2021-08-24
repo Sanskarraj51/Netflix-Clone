fb5336417837b7fef2a8859dca2f65bbe088f2fc
sanskarraj51 <86970756+Sanskarraj51@users.noreply.github.com>
2021-08-24 19:19:15 +0530

    Initial Commit

---
+++ firebase.js
@@ -0,0 +1,11 @@
+
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDVooKJ5t_8pCpYJ-ZQDbq4HK2B6JJKV28",
+    authDomain: "netflix-clone-9f83e.firebaseapp.com",
+    projectId: "netflix-clone-9f83e",
+    storageBucket: "netflix-clone-9f83e.appspot.com",
+    messagingSenderId: "339454087158",
+    appId: "1:339454087158:web:63a9c479f14964634b41d7",
+    measurementId: "G-ZEH7WFGFJC"
+  };
--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
 import './App.css';
+import Banner from './Components/Banner';
+import Navbar from './Components/Navbar';
+import Row from './Components/Row';
+import requests from './requests';
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+
+      <Navbar />
+      
+      <Banner />
+       
+      <Row title="NETFLIX ORIGINALS" fetchUrl={requests.fetchNetflixOriginals} 
+      isLargeRow />
+
+      <Row title="Trending now" fetchUrl={requests.fetchTrending} />
+
+      <Row title="Top Rated" fetchUrl={requests.fetchTopRated} />
+
+      <Row title="Romance Movies" fetchUrl={requests.fetchRomanceMovies} />
+
+      <Row title="Action Movies" fetchUrl={requests.fetchActionMovies} />
+
+      <Row title="Horror Movies" fetchUrl={requests.fetchHorrorMovies} />
+
+      <Row title="Comedy Movies" fetchUrl={requests.fetchComedyMovies} />
+
+      <Row title="Documentaries" fetchUrl={requests.fetchDocumentaries} />
+
+     
     </div>
   );
 }
